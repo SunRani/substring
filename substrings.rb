b93f76d83ac8737096c14e5cc9e97f7302c98fe2
Rani Son <143853515+SunRani@users.noreply.github.com>
2025-06-03 19:02:43 -0400

    Added file to create method

---
+++ substrings.rb
@@ -0,0 +1,6 @@
+def substrings(word, substring)
+  puts "Hello World!"
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+substrings("Hello", "World")
