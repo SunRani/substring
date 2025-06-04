7cdbf5593231d07cc959b69a59c075f198944b86
Rani Son <143853515+SunRani@users.noreply.github.com>
2025-06-03 21:55:10 -0400

    Hashes created, but value does not increase over 1

--- substrings.rb
+++ substrings.rb
@@ -1,6 +1,21 @@
-def substrings(word, substring)
-  puts "Hello World!"
+def substrings(word, dictionary)
+  words = Hash.new
+  strings = word.split(/[' ,]/)
+  strings = strings.map { | word | word.downcase }
+  dictionary = dictionary.map { | word | word.downcase}
+   dictionary.select do | substring |
+    strings.select do | sub |
+      if sub.include?(substring)
+      words[substring] =+ 1
+      p words[substring]
+      end
+    end
+    
+    
+  end 
+  puts words
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-substrings("Hello", "World")
+substrings("below", dictionary)
+substrings("Howdy partner, sit down! How's it going?", dictionary)
